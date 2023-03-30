12dd7778006ee5dc5113450c53cd3030509064da
Juan José Llorens <llorensjj@gmail.com>
2023-03-30 11:02:08 +0200

    Chore: Add enums OrderStatus ShipmentStatus

---
+++ src/Enums/Sales/Orders/OrderStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DescomMarket\Common\Enums\Sales;
+
+enum OrderStatus: string
+{
+    case PAYMENT_PENDING   = 'payment_pending';
+    case PAYMENT_ACCEPTED  = 'payment_accepted';
+    case PROCESSING        = 'processing';
+    case SHIPPED           = 'shipped';
+    case DELIVERED         = 'delivered';
+    case CANCELED          = 'canceled';
+    case COMPLETED         = 'completed';
+    case COMPLETED_PARTIAL = 'completed_partial';
+}
---
+++ src/Enums/Sales/Shipments/ShipmentStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DescomMarket\Common\Enums\Sales\Shippings;
+
+enum ShipmentStatus: string
+{
+    case PROCESSING = 'processing';
+    case SHIPPED    = 'shipped';
+    case DELIVERED  = 'delivered';
+    case CANCELED   = 'canceled';
+}
