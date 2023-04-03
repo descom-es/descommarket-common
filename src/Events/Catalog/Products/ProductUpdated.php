edb2352743b4b8588aaf3ded19238ef551b4e053
Cesar <cesargb@gmail.com>
2023-04-03 10:40:02 +0200

    add event with product was updated

---
+++ src/Events/Catalog/Products/ProductUpdated.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DescomMarket\Common\Events\Catalog\Products;
+
+/**
+ * El producto ha sido actualizado
+ */
+class ProductUpdated
+{
+    public function __construct(public readonly int $productId)
+    {
+    }
+}
