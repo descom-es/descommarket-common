d4c468621cd5e77fea6761624fe6cfc67ef37ea8
Cesar <cesargb@gmail.com>
2023-09-11 13:26:03 +0200

    notifications queue extended

---
+++ tests/Stubs/TestNotification.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DescomMarket\Common\Tests\Stubs;
+
+use DescomMarket\Common\Notifications\NotificationDescomMarket;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class TestNotification extends NotificationDescomMarket
+{
+    public function __construct(public readonly string $message)
+    {
+    }
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage())
+            ->subject('Test Notification')
+            ->line($this->message);
+    }
+
+
+}
